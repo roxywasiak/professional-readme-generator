0282dc1daaee74d2dfef88f3ed483a063bda4996
roxywasiak <ruksclone@hotmail.com>
2022-06-01 21:32:15 +0100

    added my questions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
-console.log("hello from node JS");
+//1 import inquirer library//
+const inquirer = require("inquirer");
+
+//2 my questions to prompt the user //need the readme guide first
+const mainQuestions = [
+  {
+    type: "input",
+    name: "Title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "Description",
+    message: "Enter a description of your project",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Does your project require an installation process?",
+  },
+  {
+    type: "input",
+    name: "Usage",
+    message: "How do you use your application",
+  },
+  {
+    type: "list",
+    name: "Licence",
+    message: "Does your project require a license?",
+  },
+  {
+    type: "input",
+    name: "Contributers",
+    messages: "Please enter any contributers",
+  },
+  {
+    type: "input",
+    name: "Tests",
+    message: "Enter the test process for your application",
+  },
+  {
+    type: "input",
+    name: "Email",
+    message: "Please enter your email",
+  },
+];
+
+// 3. declare your init function to ask questions
+const init = async () => {
+  // get answers for first set of questions
+  const answers = await inquirer.prompt(questions);
+
+  // display answers
+  console.log(answers);
+};
+
+// 4. start your application
+init();
